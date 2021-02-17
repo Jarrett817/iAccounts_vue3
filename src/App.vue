3b7dfb04730644dc79da6f061de6bed28598dea5
Jarrett <1206905682@qq.com>
2021-02-17 20:45:07 +0800

    chore: 引入vant ui

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'App',
-});
+export default defineComponent({ name: 'App' });
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from '@/router';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 const app = createApp(App);
 app.use(router);
+app.use(Vant);
 app.mount('#app');
