b5aa3699a995115e8ff2fb98b435413fb04feb51
Jarrett <1206905682@qq.com>
2021-04-09 21:50:43 +0800

    feat: 账单修改&&记账标签修改路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import { Home, User, Statistic, Tags } from "@/views/";
+import { Home, User, Statistic, Tags, BillEdit, TagEdit } from "@/views/";
 const routes: Array<RouteRecordRaw> = [
   { path: "/", component: Home },
+  { path: "/billList/edit/:id", component: BillEdit, props: true },
   { path: "/statistic", component: Statistic },
   { path: "/User", component: User },
-  { path: "/tags", component: Tags }
+  { path: "/tags", component: Tags },
+  { path: "/tags/edit/:id", component: TagEdit, props: true }
 ];
 export const router = createRouter({
   history: createWebHashHistory(),
---
+++ src/views/edit/bill.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>修改账单</div>
+  <i-tabbar></i-tabbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: {},
+  props: {
+    id: {
+      type: Number,
+      default: null
+    }
+  },
+  setup() {}
+});
+</script>
+
+<style lang="scss" scoped></style>
